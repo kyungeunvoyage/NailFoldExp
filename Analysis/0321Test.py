e0eb2bc84f3b5e4deb1cbbbe5ba7958f876b0b3e
kyungeunvoyage <kyungeunee@mme.dongguk.edu>
2026-04-05 08:40:09 -0400

    update structure

--- Analysis/0321Test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# 그래프 스타일 설정
-sns.set_theme(style="whitegrid")
-plt.rcParams['figure.figsize'] = (15, 6)
